e87271a47b9f28faf2e45ca2193cb085f230e3f4
Moane <moane.prates@gmail.com>
2022-06-28 16:45:37 -0300

    Criando novo teste

---
+++ ClassLibrary1/ClassLibrary1/Class2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class2
+    {
+        public string testando { get; set; }
+
+        public string outravariavel { get; set; }
+    }
+}
